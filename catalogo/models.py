116f227d910c00d4347b181350da46663278bd47
stxtiz <basty46@gmail.com>
2025-09-03 23:21:18 -0400

    Agregar nuevos servicios de alimentación y actualizar vistas y plantillas

--- catalogo/models.py
+++ catalogo/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 catalogo1 = {
     'Alimentación transportada':['transporte.jpg'],
-    'Servicios de alimentación tradicional (presencial)':['presencial.jpg'],
+    'Alimentación tradicional (presencial)':['presencial.jpg'],
+    'Concesión de Casinos':['casino.jpg'],
+    'Coffee break y eventos':['cafeb.jpg'],
+    'Repostería y snack con tickets':['snack.jpg'],
 }
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from catalogo import models as datos
 # Create your views here.
-def catalogo(request):
+def catalogoServicios(request):
     data = {
         'catalogo_servicios': datos.catalogo1
     }
