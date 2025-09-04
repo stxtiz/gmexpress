5124f6b31b01406798699763ec7cd542e93d7ce5
stxtiz <basty46@gmail.com>
2025-09-03 22:08:43 -0400

    as

--- gmexpress/urls.py
+++ gmexpress/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+
 from empresa import views as vista
+from catalogo import views as catalogo_vista
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', vista.inicio, name='inicio'),
+    path('catalogo/', catalogo_vista.catalogo, name='catalogo'),
 ]
