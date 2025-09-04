31ff99aad745e1d2c288698ec8149be0ca996471
stxtiz <basty46@gmail.com>
2025-09-03 22:08:58 -0400

    Agregar modelo de catálogo, vistas, estilos CSS y plantilla HTML para servicios de alimentación

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+catalogo1 = {
+    'Alimentación transportada':['transporte.jpg'],
+    'Servicios de alimentación tradicional (presencial)':['presencial.jpg'],
+}
--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from catalogo import models as datos
 # Create your views here.
+def catalogo(request):
+    data = {
+        'catalogo_servicios': datos.catalogo1
+    }
+    return render(request, 'templateCatalogo/catalogo1.html', data)
