549465f61b6451b55b28f2840debdad52098fb32
[200~git config --global user.name V333Igit config --global user.name [200~ <vi7o@hotmail.es>
2025-09-03 22:03:24 -0400

    cambios nuevos

--- gmexpress/settings.py
+++ gmexpress/settings.py
@@ -110,6 +110,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
