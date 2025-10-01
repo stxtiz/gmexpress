ed036fa2a661d8292d216abfed45867eae1bb79b
stxtiz <basty46@gmail.com>
2025-10-01 20:56:57 -0300

    Actualizar configuración de la base de datos a MySQL, simplificar estructura de navegación en inicio.html y limpiar estilos en CSS.

--- gmexpress/settings.py
+++ gmexpress/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'gmexpress.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gmexpress',
         'USER': 'administrador',
         'PASSWORD': '12345',
