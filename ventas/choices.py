0be7cba8e1f32a12e64c538fdc83a4ed5c100a0c
ItzYul56 <yulianoaraos14@gmail.com>
2025-10-02 19:35:09 -0300

    asdsdasd

--- ventas/choices.py
+++ ventas/choices.py
@@ -1,2 +1,15 @@
 #base de datos : tabla ventas
-estado = ('1', 'Pendiente'), ('2', 'En Proceso'), ('3', 'Completado'), ('4', 'Cancelado')
+estado = (
+    ('1', 'Pendiente'), 
+    ('2', 'En Proceso'), 
+    ('3', 'Completado'), 
+    ('4', 'Cancelado'),
+    ('5', 'Devuelto')
+    )
+
+tipo_venta = (
+    ('1', 'Producto'),
+    ('2', 'Servicio'),
+    ('3', 'Mixto')
+)
+
--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.utils import timezone
+from ventas.choices import estado, tipo_venta
 # Create your models here.
+
+class Venta(models.Model):
+    num_venta = models.CharField(max_length=45, unique=True, null=False)
+    cantidad_items = models.IntegerField(null=False)
+    descripcion = models.CharField(max_length=100, null=False)
+    fecha_venta = models.DateTimeField(default=timezone.now)
+    monto_total = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    estado = models.CharField(max_length=1, choices=estado, default='1')
+    tipo_venta = models.CharField(max_length=1, choices=tipo_venta, default='1')
+    
+    
+    class Meta:
+        db_table = "ventas"
+        verbose_name = "Venta"
+        verbose_name_plural = "Ventas"
+        
+    def __str__(self):
+        return f" {self.num_venta} - {self.cantidad_items} - {self.descripcion} - {self.fecha_venta} - {self.monto_total} - {self.estado} - {self.tipo_venta}"
+
+class DetalleVenta(models.Model):
+    venta = models.ForeignKey(Venta, on_delete=models.CASCADE)
+    producto_servicio = models.CharField(max_length=100, null=False)
+    cantidad = models.IntegerField(null=False)
+    precio_unitario = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    subtotal = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    
+    class Meta:
+        db_table = "detalle_ventas"
+        verbose_name = "Detalle Venta"
+        verbose_name_plural = "Detalles Ventas"
+        
+    def __str__(self):
+        return f" {self.venta.num_venta} - {self.producto_servicio} - {self.cantidad} - {self.precio_unitario} - {self.subtotal}"
