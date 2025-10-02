cbb0a832b7b81a349af3c901dff8f40d4be577ad
[200~git config --global user.name V333Igit config --global user.name [200~ <vi7o@hotmail.es>
2025-10-02 20:44:38 -0300

    Agregar clase productoAdmin y registrar modelo producto en el administrador

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import producto, categoria, servicio
 # Register your models here.
+
+class productoAdmin(admin.ModelAdmin):
+    list_display = [
+        'idproducto',
+        'nombre',
+        'descripcion',
+        'precio',
+        'stock',
+        'imagen',
+        'categoria_idcategoria',
+        '__str__'
+    ]
+
+
+admin.site.register(producto, productoAdmin)
+
