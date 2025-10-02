d4e1f28695394a9f62cf81af4ddf4dbb0009da55
stxtiz <basty46@gmail.com>
2025-10-02 20:39:38 -0300

    Registrar modelos 'Venta' y 'DetalleVenta' en el administrador de Django

--- ventas/admin.py
+++ ventas/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import Venta, DetalleVenta
 # Register your models here.
+
+
+class DetalleVentaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'descripcion', 'cantidad_items', 'monto_total', 'fecha_venta', '__str__']
+    
+    
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'num_venta', 'estado', 'tipo_venta', 'detalleVenta', '__str__']
+    
+    
+admin.site.register(DetalleVenta, DetalleVentaAdmin)
+admin.site.register(Venta, VentaAdmin)
